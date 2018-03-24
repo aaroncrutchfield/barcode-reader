d93e550789697db5c32d7ba4ddc8de70ea337b35
Aaron Crutchfield <aaroncrutchfield09@gmail.com>
2018-03-24 17:38:40 -0400

    fix - Remove remaining code from Room implementation

--- app/src/main/java/com/google/android/gms/samples/vision/barcodereader/data/Part.java
+++ app/src/main/java/com/google/android/gms/samples/vision/barcodereader/data/Part.java
@@ -1,9 +1,6 @@
 package com.google.android.gms.samples.vision.barcodereader.data;
 
 
-import android.arch.persistence.room.ColumnInfo;
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
 
@@ -11,13 +8,10 @@ import android.support.annotation.NonNull;
  * Created by ioutd on 12/1/2017.
  */
 
-@Entity
 public class Part {
-    @PrimaryKey
     @NonNull
     private String serial;
 
-    @ColumnInfo(name = "partnumber")
     private String partnumber;
     private int quantity;
 
